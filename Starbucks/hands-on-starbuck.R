d3d7b4d6dbf6290d2baeaddd9beef5f6de044889
Aldi Irianto <aldiirianto999@gmail.com>
2022-10-09 00:44:05 +0700

    mengupload file - file

---
+++ Starbucks/hands-on-starbuck.R
@@ -0,0 +1,15 @@
+---
+  title: "Starbuck Location Worldwide 2021 Version"
+  author: "Aldi Irianto"
+  date: "October 08th, 2022"
+  output:
+    html_document:
+    theme: cosmo
+  highlight: tango
+  toc: true
+  toc_float:
+    collapsed: true
+  df_print: paged
+---
+  
+  
